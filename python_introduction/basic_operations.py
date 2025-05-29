c6a4ed02f9f7d2c86cf3c3d0a9c29376fe85625c
Godwinakladzi <godwinakladzi@gmail.com>
2025-05-29 11:02:40 +0100

    added multiplication

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,8 @@
-number1 = 5
-number2 = 10
+number1 = 10
+number2 = 5
 sum = number1 + number2
 difference_result = number1 - number2
+product = number1 * number2
 print("Sum:", sum)
 print("Difference:", difference_result)
+print("Product:", product)
