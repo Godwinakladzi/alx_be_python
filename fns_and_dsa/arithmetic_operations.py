2193d77e563fea76461980090c2655c3694f8d82
Godwinakladzi <godwinakladzi@gmail.com>
2025-06-08 02:41:30 +0100

    handled the division logic

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -10,12 +10,10 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
-        if num2 != 0:
-            return num1 / num2
-        else:
+        if num2 == 0:
             return "Error: Division by zero is not allowed."
-    else:
-        return "Error: Invalid operation."
+        else:
+            return num1 / num2
     
 result = perform_operation(num1, num2, operation)
 print(f"The result of {operation}ing {num1} and {num2} is: {result}")
